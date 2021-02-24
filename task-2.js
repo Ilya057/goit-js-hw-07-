f5ec4ede744fc84d9f8543e9899bb189d3cdc1eb
Ilya Nikonenko <ilyanikonenko1613@gmail.com>
2021-02-24 15:29:01 +0200

    .

--- task-2.js
+++ task-2.js
@@ -1,13 +1,13 @@
-const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
+// const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы" ];
 
-const listRef = document.getElementById("ingredients");
-console.log(listRef);
+// const listRef = document.getElementById("ingredients");
+// console.log(listRef);
 
-ingredients.forEach((elem, index) => {
-  const item = document.createElement("li");
-  console.log(item);
-  item.textContent = elem;
-  listRef.append(item);
-});
+// ingredients.forEach((elem, index) => {
+//   const item = document.createElement("li");
+//   console.log(item);
+//   item.textContent = elem;
+//   listRef.append(item);
+// });
 
-listRef.style.listStyle = " none";
+// listRef.style.listStyle = " none";
