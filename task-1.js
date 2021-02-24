229c2f10ed67f674d533a654c6ed5cfaf600d053
Ilya Nikonenko <ilyanikonenko1613@gmail.com>
2021-02-24 14:24:09 +0200

    created task 2

--- task-1.js
+++ task-1.js
@@ -1,11 +1,14 @@
-const listRef = document.querySelectorAll("#categories li.item");
-console.log(listRef);
-const listLength = listRef.length;
-console.log(`В списке ${listLength} категории`);
+// const listRef = document.querySelectorAll("#categories li.item");
+// console.log(listRef);
+// const listLength = listRef.length;
+// console.log(`В списке ${listLength} категории`);
 
-listRef.forEach((elem, index) => {
-  const h2 = document.querySelectorAll("h2");
-  console.log(`Категория ${h2[index].textContent}`);
-  const li = document.querySelectorAll("li.item ul ");
-  console.log(`Количество: ${li[index].children.length}`);
-});
+// listRef.forEach((elem, index) => {
+//   const h2 = document.querySelectorAll("h2");
+//   console.log(`Категория ${h2[index].textContent}`);
+//   const li = document.querySelectorAll("li.item ul ");
+//   console.log(`Количество: ${li[index].children.length}`);
+// });
+
+
+
---
+++ task-2.js
@@ -0,0 +1,13 @@
+const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
+
+const listRef = document.getElementById("ingredients");
+console.log(listRef);
+
+ingredients.forEach((elem, index) => {
+  const item = document.createElement("li");
+  console.log(item);
+  item.textContent = elem;
+  listRef.append(item);
+});
+
+listRef.style.listStyle = " none";
